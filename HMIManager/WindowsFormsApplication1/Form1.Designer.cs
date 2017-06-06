e71615d532ee933d8e8941e0cbe52913d660e456
moJJqq <mehrdadq@gmail.com>
2017-06-06 02:09:43 -0700

    fef
    sddddddddddddddddd

--- HMIManager/WindowsFormsApplication1/Form1.Designer.cs
+++ HMIManager/WindowsFormsApplication1/Form1.Designer.cs
@@ -39,6 +39,7 @@
             this.lblError = new System.Windows.Forms.Label();
             this.btnStartAutoRead = new System.Windows.Forms.Button();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
@@ -133,11 +134,22 @@
             this.statusStrip1.TabIndex = 10;
             this.statusStrip1.Text = "statusStrip1";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(217, 218);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(416, 327);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.btnStartAutoRead);
             this.Controls.Add(this.lblError);
@@ -167,6 +179,7 @@
         private System.Windows.Forms.Label lblError;
         private System.Windows.Forms.Button btnStartAutoRead;
         private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
