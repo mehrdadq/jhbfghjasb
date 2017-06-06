65d02fd33ae8f8312f917486ab29d6e42491b6c9
moJJqq <mehrdadq@gmail.com>
2017-06-06 13:55:53 +0430

    remove Button

--- HMIManager/WindowsFormsApplication1/Form1.cs
+++ HMIManager/WindowsFormsApplication1/Form1.cs
@@ -186,5 +186,10 @@ namespace testpanel
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
                     }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
