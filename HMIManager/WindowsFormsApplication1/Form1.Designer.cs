33a69a92c8d4eafe5d4f8aca4628791c063e1f41
moJJqq <mehrdadq@gmail.com>
2017-06-06 11:42:40 +0430

    Add files via upload

---
+++ HMIManager/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace testpanel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Timer timer1;
+            this.btnReadManual = new System.Windows.Forms.Button();
+            this.btnLoadDevice = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.btnStartAutoRead = new System.Windows.Forms.Button();
+            timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnReadManual
+            // 
+            this.btnReadManual.Location = new System.Drawing.Point(34, 41);
+            this.btnReadManual.Margin = new System.Windows.Forms.Padding(2);
+            this.btnReadManual.Name = "btnReadManual";
+            this.btnReadManual.Size = new System.Drawing.Size(108, 27);
+            this.btnReadManual.TabIndex = 0;
+            this.btnReadManual.Text = "Raed DataManual";
+            this.btnReadManual.UseVisualStyleBackColor = true;
+            this.btnReadManual.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLoadDevice
+            // 
+            this.btnLoadDevice.Location = new System.Drawing.Point(34, 73);
+            this.btnLoadDevice.Name = "btnLoadDevice";
+            this.btnLoadDevice.Size = new System.Drawing.Size(108, 23);
+            this.btnLoadDevice.TabIndex = 2;
+            this.btnLoadDevice.Text = "Load Device";
+            this.btnLoadDevice.UseVisualStyleBackColor = true;
+            this.btnLoadDevice.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(34, 102);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(108, 134);
+            this.listBox1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "IP:";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtIP.Location = new System.Drawing.Point(172, 45);
+            this.txtIP.Margin = new System.Windows.Forms.Padding(2);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(206, 19);
+            this.txtIP.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Status:";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblError.Location = new System.Drawing.Point(120, 9);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 13);
+            this.lblError.TabIndex = 8;
+            // 
+            // btnStartAutoRead
+            // 
+            this.btnStartAutoRead.BackColor = System.Drawing.SystemColors.Window;
+            this.btnStartAutoRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnStartAutoRead.Location = new System.Drawing.Point(217, 116);
+            this.btnStartAutoRead.Name = "btnStartAutoRead";
+            this.btnStartAutoRead.Size = new System.Drawing.Size(114, 78);
+            this.btnStartAutoRead.TabIndex = 9;
+            this.btnStartAutoRead.Text = "Start";
+            this.btnStartAutoRead.UseVisualStyleBackColor = false;
+            this.btnStartAutoRead.Click += new System.EventHandler(this.btnStartAutoRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 327);
+            this.Controls.Add(this.btnStartAutoRead);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnLoadDevice);
+            this.Controls.Add(this.btnReadManual);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Software Towzin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadManual;
+        private System.Windows.Forms.Button btnLoadDevice;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Button btnStartAutoRead;
+    }
+}
+
---
+++ HMIManager/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using EasyModbus;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace testpanel
+{
+    public partial class Form1 : Form
+    {
+        int CounterList = 0;
+        bool timerActive = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtIP.Text != "")
+                {
+                    
+                    ModbusClient svimaster = new ModbusClient(txtIP.Text, 502);
+                    lblError.Text = "connect " + txtIP.Text;
+                    svimaster.Connect();
+                    int[] Main_ProductCode;
+                    lblError.Text = "connect "+ txtIP.Text+" ok";
+                    bool[] checkbit = svimaster.ReadCoils(0, 1);// Check bit send from HMI
+                    //Read Data From HMI
+                    if (checkbit[0] == true)
+                    {
+                        int[] Main_Product_Code = svimaster.ReadHoldingRegisters(2, 12);
+                        int[] Weight = svimaster.ReadHoldingRegisters(105, 2);
+                        int[] Datee = svimaster.ReadHoldingRegisters(101, 2);
+                        int[] Timee = svimaster.ReadHoldingRegisters(103, 2);
+                        int[] Oerator_num = svimaster.ReadHoldingRegisters(111, 1);
+                        int[] Kind = svimaster.ReadHoldingRegisters(112, 1);//نوع محصول
+                        int[] Samplee = svimaster.ReadHoldingRegisters(109, 2);//نمونه
+                        int[] Order_numberr = svimaster.ReadHoldingRegisters(107, 2);
+                        int[] Product_Code = svimaster.ReadHoldingRegisters(19, 12);
+                        int[] Max_Address = svimaster.ReadHoldingRegisters(99, 1);//ماکزیمم آدرس استفاده شده
+                        int[] Max_send = svimaster.ReadHoldingRegisters(100, 1);//ماکزیمم آدرس ارسال شده
+                        float NetWeightHMI = ModbusClient.ConvertRegistersToFloat(Weight);//وزن 
+                        float SampleHMI = ModbusClient.ConvertRegistersToFloat(Samplee);//نمونه
+                        double DateHMI = ModbusClient.ConvertRegistersToDouble(Datee);//تاریخ
+                        double TimeHMI = ModbusClient.ConvertRegistersToDouble(Timee);//ساعت
+                        string productcodeHMI = ModbusClient.ConvertRegistersToString(Product_Code, 0, 12);//کد محصول
+                        float Order_numberHMI = ModbusClient.ConvertRegistersToFloat(Order_numberr);//شماره سفارش
+                        DateTime Date = Convert.ToDateTime("20" + DateHMI.ToString().Substring(0, 2) + "/" + DateHMI.ToString().Substring(2, 2) + "/" + DateHMI.ToString().Substring(4, 2) + " " + TimeHMI.ToString().Substring(2, 2) + ":" + TimeHMI.ToString().Substring(4, 2) + ":" + TimeHMI.ToString().Substring(6, 2));
+                        /*Reply From SQL Sever
+                        Status=0 Unable to Saved to SQL Server
+                        Status=1 Save to SQL Server
+                        Status=2 Error in Data*/
+                       
+                        int Status = 1;//جواب سرور
+                        svimaster.WriteSingleRegister(113, Status);
+                        lblError.Text = "  Product =" + productcodeHMI + "Net Weight=" + NetWeightHMI.ToString();
+                        checkbit[0] = false;
+
+                    }
+                    // Write to Panels
+                    int Remain_Product = 100;//محصول باقیمانده
+                    svimaster.WriteSingleRegister(114, Remain_Product);
+                    float Main_order = 1;//شماره سفارش از سروربه پنل
+                    int[] floatreg = ModbusClient.ConvertFloatToTwoRegisters(Main_order);
+                    svimaster.WriteMultipleRegisters(0, floatreg);
+                    string Main_prod_code = "uyuy";//کد کالا از سرور به پنل
+                    Main_ProductCode = ModbusClient.ConvertStringToRegisters(Main_prod_code);
+                    svimaster.WriteMultipleRegisters(2, Main_ProductCode);
+                }
+                else
+                {
+                    lblError.Text = "Please Fill IP Address";
+                }
+            }
+            catch
+            {
+                lblError.Text = "Error Read Data From Device";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(@"Data Source=192.168.1.11\Towzin;Initial Catalog=Towzin;User ID=towzin;Password=123456"))
+                using (SqlCommand command = new SqlCommand("select * from Devices", connection))
+                {
+                    connection.Open();
+                    listBox1.Items.Clear();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            listBox1.Items.Add(reader["IP"].ToString());
+
+                        }
+                    }
+                }
+                CounterList = 0;
+            }
+            catch
+            {
+                lblError.Text = "Error Read Device From Database";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (listBox1.Items.Count != 0 &  CounterList==0)
+            {
+                CounterList = listBox1.Items.Count;
+            }
+            try
+            {
+               if (CounterList != 0 & timerActive==true)
+                {
+                    ModbusClient svimaster = new ModbusClient(listBox1.Items[CounterList - 1].ToString(), 502);
+                    lblError.Text = "Read Device " + listBox1.Items[CounterList - 1].ToString();
+                    CounterList = CounterList - 1;
+                }
+            }
+                
+            catch
+            {
+                lblError.Text = "Error Read Data From Device";    
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(@"Data Source=192.168.1.11\Towzin;Initial Catalog=Towzin;User ID=towzin;Password=123456"))
+                using (SqlCommand command = new SqlCommand("select * from Devices", connection))
+                {
+                    connection.Open();
+                    listBox1.Items.Clear();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            listBox1.Items.Add(reader["IP"].ToString());
+
+                        }
+                    }
+                }
+                CounterList = 0;
+            }
+            catch
+            {
+                lblError.Text = "Error Read Device From Database";
+            }
+        }
+
+        private void btnStartAutoRead_Click(object sender, EventArgs e)
+        {
+            
+            if(btnStartAutoRead.Text=="Start")
+            {
+                btnStartAutoRead.BackColor = Color.Red;
+                timerActive = true;
+                btnStartAutoRead.Text = "Stop";
+               
+            }  
+            else
+            {
+                timerActive = false;
+                btnStartAutoRead.BackColor = Color.White;
+                btnStartAutoRead.Text = "Start";
+                lblError.Text = "";
+                 
+            }
+        }
+    }
+}
